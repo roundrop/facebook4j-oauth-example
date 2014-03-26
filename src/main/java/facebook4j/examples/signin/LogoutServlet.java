d715728b4f4522d22080042303bd4fc7e765c7a2
zipz <anders.bergvall@gmail.com>
2014-03-26 23:02:30 +0100

    Added Facebok response redirect.

--- src/main/java/facebook4j/examples/signin/LogoutServlet.java
+++ src/main/java/facebook4j/examples/signin/LogoutServlet.java
@@ -11,8 +11,16 @@ public class LogoutServlet extends HttpServlet {
     private static final long serialVersionUID = 5357658337449255998L;
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {      
+        Facebook facebook = (Facebook) request.getSession().getAttribute("facebook");
+        String accessToken = "";
+        try {
+        	accessToken = facebook.getOAuthAccessToken().getToken();
+        } catch (Exception e) {
+            throw new ServletException(e);
+        }
         request.getSession().invalidate();
-        response.sendRedirect(request.getContextPath()+ "/");
+        //siteurl.com should match your facebook app settings
+        response.sendRedirect("http://www.facebook.com/logout.php?next=http://www.siteurl.com/index.html&access_token=" + accessToken);
     }
 }
