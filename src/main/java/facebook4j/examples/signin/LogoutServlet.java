7f926127a44be224448f9fe7fee7906d68610fc6
roundrop <roundrop@gmail.com>
2014-03-28 17:57:36 +0900

    refactoring #1

--- src/main/java/facebook4j/examples/signin/LogoutServlet.java
+++ src/main/java/facebook4j/examples/signin/LogoutServlet.java
@@ -1,11 +1,12 @@
 package facebook4j.examples.signin;
 
-import java.io.IOException;
+import facebook4j.Facebook;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class LogoutServlet extends HttpServlet {
     private static final long serialVersionUID = 5357658337449255998L;
@@ -20,7 +21,11 @@ public class LogoutServlet extends HttpServlet {
             throw new ServletException(e);
         }
         request.getSession().invalidate();
-        //siteurl.com should match your facebook app settings
-        response.sendRedirect("http://www.facebook.com/logout.php?next=http://www.siteurl.com/index.html&access_token=" + accessToken);
+
+        // Log Out of the Facebook
+        StringBuffer next = request.getRequestURL();
+        int index = next.lastIndexOf("/");
+        next.replace(index+1, next.length(), "");
+        response.sendRedirect("http://www.facebook.com/logout.php?next=" + next.toString() + "&access_token=" + accessToken);
     }
 }
